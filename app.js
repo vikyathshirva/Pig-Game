11c8f6651e26a0e316f262a53b82edc4037ec530
vikyathshirva <vicky.fames@gmail.com>
2020-06-24 10:34:19 +0530

    test for change

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
+- Th first player to reach 100 points on GLOBAL score wins the game
+- example for change in git
 */
 
 var scores, roundScores, dice;
