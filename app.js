cb3ef7e425d0c2ff392e5e2604b47b4912435bc2
vikyathshirva <vicky.fames@gmail.com>
2020-06-24 21:13:12 +0530

    Javascript of the game in the process

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
-* GAME RULES: - The game has 2 players, playing in rounds
+/* GAME RULES: - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - Th first player to reach 100 points on GLOBAL score wins the game
 - example for change in git
 - another example
+- this is a test for branching
 */
 
-var scores, roundScores, dice;
+var scores, roundScores,activePlayer ;
 
 scores = [0,0];
 roundScores = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random()*6)+1;
-console.log(dice);
-
-
-document.querySelector('#current-'+ activePlayer).textContent = dice;
-//document.querySelector('#current-'+activePlayer).innerHTML = '<em>'+ dice +'</em>';
-
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 
 
+document.querySelector('.btn-roll').addEventListener('click', function () {
+
+
+    var dice = Math.floor(Math.random()*6)+1;
+
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice +'.png';
+});
 
 
 
